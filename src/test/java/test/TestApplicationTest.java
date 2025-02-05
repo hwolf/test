677d4032933c891426da894fd3707c5e147c7093
hwolf <heiko>
2025-02-05 22:41:41 +0100

    introduce bug

--- src/test/java/test/TestApplicationTest.java
+++ src/test/java/test/TestApplicationTest.java
@@ -3,10 +3,13 @@ package test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.assertj.core.api.Assertions.fail;
+
 @SpringBootTest
 class TestApplicationTest {
 
     @Test
     void contextLoads() {
+        fail();
     }
 }
