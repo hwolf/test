9462986598a5fa6abe6fe5b80e7cd7a33034a720
hwolf <heiko>
2025-02-05 22:43:51 +0100

    fix test

--- src/test/java/test/TestApplicationTest.java
+++ src/test/java/test/TestApplicationTest.java
@@ -3,13 +3,10 @@ package test;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.Assertions.fail;
-
 @SpringBootTest
 class TestApplicationTest {
 
     @Test
     void contextLoads() {
-        fail();
     }
 }
