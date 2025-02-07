8b71d6dcd51df24b8dedc73169c3092f28558f94
hwolf <heiko>
2025-02-07 01:38:22 +0100

    Test

--- src/main/java/test/TestApplication.java
+++ src/main/java/test/TestApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TestApplication {
 
     public static void main(String[] args) {
-        String x = null;
-        x.length();
         SpringApplication.run(TestApplication.class, args);
     }
 }
